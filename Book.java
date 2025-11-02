6c3ea4db12a447d9c811f0c9b9f17b611a31d719
Farjana Jim <fjim4615@gmail.com>
2025-11-02 21:18:13 +0600

    Add files via upload
    String

--- Book.java
+++ Book.java
@@ -1,25 +1,23 @@
 
-package newpackage;
+package stringpackage;
+
+import java.util.Scanner;
 
 
 public class Book {
-
-    
-    String title;
-    String author;
-    int year ;
-    
-    private static String genre="Fiction";
-    private static int totalBooks=0;
-    
-    public Book(String title,String author,int year){
+   String title;
+   String author;
+   int year;
+   static String genre="Horror"; 
+   private static int totalBooks=0;
+   
+   public Book(String title,String author,int year){
         this.title=title;
         this.author=author;
         this.year=year;
         totalBooks++;         
     }
-    
-    public void displayDetails(){
+   public void displayDetails(){
         System.out.println("Title: "+title+" Author name: "+author+" Year: "+year+" Genre: "+genre);
     }
     
@@ -27,21 +25,31 @@ public class Book {
         return totalBooks;
         
     }
-    public static void main(String[] args){
-        Book s1=new Book("Harry Potter","J. K. Rowling",1991);
-        Book s2=new Book("The Alchemist","Paulo Coelho",1988);
-        Book s3=new Book("Nineteen Eighty-Four","Secker & Warburg",1949);
+    
+    public static void main(String args[]){
+        Book n[]=new Book[2];
+        Scanner sc=new Scanner(System.in);
         
-        
-        s1.displayDetails();
-        s2.displayDetails();
-        s3.displayDetails();
-        
-        System.out.println("Total num of books: "+Book.getTotalBooks());
-        
-        
+        for(int i=0;i<2;i++){
+            System.out.println("Enter details for the book: "+(i+1)+"");
+            System.out.println("Enter title: ");
+            String t=sc.nextLine();
+            System.out.println("Enter Author name: ");
+            String a=sc.nextLine();
+            System.out.println("Enter year: ");
+            int y=sc.nextInt();
+            sc.nextLine();
+            
+            n[i]=new Book(t,a,y);
+            
+        }
+        for(int i=0;i<2;i++){
+            n[i].displayDetails();
+        }
+        System.out.println("Total num of Books: "+getTotalBooks());
+    }
         
         
     }
-       
-}
+    
+
