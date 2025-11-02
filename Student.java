d31668d54173191d4e566df1a5cc3ff73c780900
Farjana Jim <fjim4615@gmail.com>
2025-11-02 21:11:36 +0600

    String

--- Student.java
+++ Student.java
@@ -1,23 +1,25 @@
 
-package newpackage;
+package stringpackage;
 
+import java.util.Scanner;
 
 public class Student {
     int id;
     String name;
-    String department; 
-    double cgpa;
-    private static String university="Ulab";
+    String department;
+    double cgpa; 
+    private static String university="'Ulab";
+    
     private static int totalStudent=0;
-    
-    
-    public Student(int id,String name,String department,double cgpa){
-        this.id=id;
-        this.name=name;
-        this.department=department;
-        this.cgpa=cgpa; 
+
+    public Student(int id, String name, String department, double cgpa) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.cgpa = cgpa;
         totalStudent++;
     }
+    
     
     public void displayDetails(){
         System.out.println("ID: "+id+" Name: "+name+" Department: "+department+" Cgpa: "+cgpa+" University: "+university);
@@ -26,25 +28,34 @@ public class Student {
         return totalStudent;
         
     }
-    
-    
-    
-    
     public static void main(String[] args){
-        Student s1=new Student(241,"Jim","CSE",3.85);
-        Student s2=new Student(243,"Joty","MSJ",3.50);
-        Student s3=new Student(210,"Nody","BBA",3.75);
-        
-        
-        s1.displayDetails();
-        s2.displayDetails();
-        s3.displayDetails();
-        
-        System.out.println("Total num of Student: "+Student.getTotalStudent());
-        
-        
-        
-        
+       Student s[]=new Student[3];
+       Scanner sc=new Scanner(System.in);
+    
+    
+    for(int i=0;i<3;i++){
+     System.out.println("Enter details for the students: "+(i+1)+"");
+  
+     System.out.println("Enter student ID: ");
+     int id=sc.nextInt();
+     sc.nextLine();
+     System.out.println("Enter student Name: ");
+     String name=sc.nextLine();
+     System.out.println("Enter department: ");
+     String department=sc.nextLine();
+     System.out.println("Enter CGPA: ");
+     double cgpa=sc.nextDouble();
+     sc.nextLine();
+     
+     s[i]=new Student(id,name,department,cgpa);
+    }
+    for(int i=0;i<3;i++){
+        s[i].displayDetails();
+    }
+    System.out.println("Total num of Student: "+getTotalStudent());
     }
     
-}
+
+    }
+    
+
